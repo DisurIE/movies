c8de73cee2977499add2f6da82245e53a56be7b8
Ivan <kulkov_2003@bk.ru>
2023-07-04 10:16:01 +0300

    add conditions and cycles

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+"use strict"
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '')
 
 const personalMovieDB = {
@@ -7,13 +8,27 @@ const personalMovieDB = {
     genres: [],
     private: false
 };
-
-const firstMovieSeen = prompt('Один из последних просмотренных фильмов', ''),
-      firstRateMovie = prompt('На сколько оцените его?', ''),
-      secondMovieSeen = prompt('Один из последних просмотренных фильмов', ''),
-      secondRateMovie = prompt('На сколько оцените его?', '');
-
-    personalMovieDB.movies[firstMovieSeen] = firstRateMovie;
-    personalMovieDB.movies[secondMovieSeen] = secondRateMovie,
+    if(personalMovieDB.count < 10){
+        alert("Просмотрено мало фильмов");
+    }
+    else if(personalMovieDB.count > 10 && personalMovieDB.count < 30){
+        alert("Вы классический зритель");
+    }
+    else{
+        alert("Вы киноман");
+    }
+    for(let i = 0; i < 2; i++){
+        const firstMovieSeen = prompt('Один из последних просмотренных фильмов', ''),
+              firstRateMovie = prompt('На сколько оцените его?', '');
+              if(firstMovieSeen == '' || firstRateMovie == '' 
+                || firstRateMovie.length > 50 || firstMovieSeen.length > 50){
+                    alert("Ошибка при вводе")
+                    i--;
+              }
+              else{
+                personalMovieDB.movies[firstMovieSeen] = firstRateMovie;
+              }
+    }
+      
 
 console.log(personalMovieDB)
